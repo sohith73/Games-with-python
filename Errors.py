6032d301eef203c9e5fd2946b25ed8582bfe416b
Sohith Bandreddi <sohith73@gmail.com>
2023-08-17 16:29:28 +0530

    try and except exception handling

---
+++ Errors.py
@@ -0,0 +1,7 @@
+# print(1/0)# giving error
+try:
+    print(1/0)
+except:
+    print("There is something wrong please check the error in the code and try again brother")
+finally:
+    print("It will run no matter what will happens in the code")
