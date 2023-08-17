181f20c20f64aae525c23e7ed5c5bba08fb807c7
Sohith Bandreddi <sohith73@gmail.com>
2023-08-17 14:27:34 +0530

    using assert for the first time and this is amazing

--- OOPS.py
+++ OOPS.py
@@ -1,6 +1,11 @@
 class Item:
 	def calculate(self, x, y ):
+		assert  x >=10 , f"value is less than ten and the value is {x}"
+		assert y >= 10, f"value is less than ten and the value is {y}"
 		return x*y+x/y
+	def __init__(self):
+		print("Object is created using constructor")
+
 
 iteam1 = Item()
-print(iteam1.calculate(109, 9))
+print(iteam1.calculate(109, 90))
